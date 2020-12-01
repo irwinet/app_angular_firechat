a71968e4598fb81adab99daf439b1a07ca8063ec
irwinet <irwinestradatorres@gmail.com>
2020-12-01 15:54:27 -0500

    Cargar y agregar mensajes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,10 +11,21 @@ export class ChatComponent  {
 
   mensaje: string = "";
 
-  constructor() { }
+  constructor(public chatService: ChatService) { 
+    this.chatService.cargarMensajes()
+                      .subscribe();
+  }
 
   enviar_mensaje(){
     console.log(this.mensaje);
+
+    if(this.mensaje.length === 0){
+      return;
+    }
+
+    this.chatService.agregarMensaje(this.mensaje)
+                    .then(()=>this.mensaje="")
+                    .catch((err)=>console.error('Error al enviar', err));
   }
 
 }
